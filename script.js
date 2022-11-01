321203df258c56cfcf7e8d4b50df640e2f7f5d84
ar78ilnik <arakchiev.ilya@yandex.ru>
2022-11-01 11:07:00 +0300

    2.12

--- script.js
+++ script.js
@@ -1,21 +1,63 @@
-let time = prompt("Введите дату в формате YYYY-MM-DD?");
-let money = prompt("Ваш бюджет на месяц?");
+'use strict';
 
-appData = {
-	budget: money,
-	timeData: time,
-	expenses: {},
-	optionalExpenses: {},
-	income: [],
-	savings: false
+let money, time;
+
+function start() {
+    time = prompt("Введите дату в формате YYYY-MM-DD?", "");
+
+    while (isNaN(money) || money == "" || money == null) {
+        money = +prompt("Ваш бюджет на месяц?", "");
+        console.log(money);
+    }
+}
+start();
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: true
+};
+
+function chooseExpenses() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+            b = prompt("Во сколько обойдется?", '');
+        console.log(b);
+        if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null && a != '' && b != '' && a.length < 50) {
+            appData.expenses[a] = b;
+        } else {
+            i--
+        }
+    }
+}
+chooseExpenses();
+
+function checkSaving() {
+    if (appData.savings == true) {
+        let save = +prompt("Какова сумма накопления?"),
+            percent = +prompt("Под какой процент?");
+
+        appData.monthIncome = save / 100 / 12 * percent;
+        console.log("Доход с вашего депозита:" + appData.monthIncome);
+    }
+}
+checkSaving();
+
+appData.moneyPerDay = (appData.budget / 30).toFixed();
+
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Ошибка");
 }
 
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a2 = prompt("Во сколько обойдется?", ''),
-	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a4 = prompt("Во сколько обойдется?", '');
-
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
-
-alert(appData.budget / 30);
+console.log(appData);
---
+++ script2.js
@@ -0,0 +1,7 @@
+'use strict';
+
+function calc(a, b) {
+    return a + b;
+}
+
+console.log(calc(3, 4));
