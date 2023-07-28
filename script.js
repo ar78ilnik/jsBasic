c7cf96a99c01782ad7d39ef003d5e2d576132cab
ar78ilnik <ar78ilnik@gmail.com>
2023-07-28 11:40:45 +0300

    Урок 2.22. Получение элементов со страницы

--- script.js
+++ script.js
@@ -1,70 +1,13 @@
-'use strict';
+let box = document.getElementById('box'),
+    btn = document.getElementsByTagName('button'), // Множественное число Elements говорит о том, что это псевдо-массив
+    circle = document.getElementsByClassName('circle'), // Множественное число Elements говорит о том, что это псевдо-массив, без точки
+    heart = document.querySelectorAll('.heart'); // этот псевдо-массив имеет только один метод forEach
 
-let money, time;
+console.log(box);
 
-function start() {
-    time = prompt("Введите дату в формате YYYY-MM-DD?", "");
+console.log(btn); // вывод всего массива
+console.log(btn[0]); // вывод отдельно элемента массива
 
-    while (isNaN(money) || money == "" || money == null) {
-        money = +prompt("Ваш бюджет на месяц?", "");
-        console.log(money);
-    }
-}
-start();
+console.log(circle[2]);
 
-let appData = {
-    budget: money,
-    timeData: time,
-    expenses: {},
-    optionalExpenses: {},
-    income: [],
-    savings: true,
-    chooseExpenses: function() {
-        for (let i = 0; i < 2; i++) {
-            let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-                b = prompt("Во сколько обойдется?", '');
-            console.log(b);
-            if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null && a != '' && b != '' && a.length < 50) {
-                appData.expenses[a] = b;
-            } else {
-                i--
-            }
-        }
-    },
-    detectDayBudget: function() {
-        appData.moneyPerDay = (appData.budget / 30).toFixed();
-        alert("Ежедневный бюджет: " + appData.moneyPerDay);
-    },
-    detectLevel: function() {
-        if (appData.moneyPerDay < 100) {
-            console.log("Минимальный уровень достатка");
-        } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-            console.log("Средний уровень достатка");
-        } else if (appData.moneyPerDay > 2000) {
-            console.log("Высокий уровень достатка");
-        } else {
-            console.log("Ошибка");
-        }
-    },
-    checkSavings: function() {
-        if (appData.savings == true) {
-            let save = +prompt("Какова сумма накопления?"),
-                percent = +prompt("Под какой процент?");
-    
-            appData.monthIncome = save / 100 / 12 * percent;
-            console.log("Доход с вашего депозита:" + appData.monthIncome);
-        }
-    },
-    chooseOptExpenses: function() {
-        for(let i = 1; i<3; i++) {
-            let opt = prompt("Статья необязательных расходов?", "");
-            appData.optionalExpenses[i] = opt;
-        }
-    },
-    chooseIncome: function() {
-        let items = prompt("Что принесет дополнительный доход", "");
-        appData.income = items.split(", ");
-        appData.income.push(prompt("Может что-то еще?"));
-        appData.income.sort();
-    }
-};
+console.log(heart);
