8987370d91d099e7639d0b9b58699a539d41791e
ar78ilnik <ar78ilnik@gmail.com>
2023-10-03 11:04:52 +0300

    4.1 Формат JSON. 4.2 AJAX

--- script.js
+++ script.js
@@ -1,58 +1,78 @@
-let box = document.getElementById('box'),
-    btn = document.getElementsByTagName('button'), // Множественное число Elements говорит о том, что это псевдо-массив
-    circle = document.getElementsByClassName('circle'), // Множественное число Elements говорит о том, что это псевдо-массив, без точки
-    heart = document.querySelectorAll('.heart'), // этот псевдо-массив имеет только один метод forEach
-    wrapper = document.querySelector('.wrapper');
+// 4.1 JSON
 
-// изменяем стили элементов
+//Формат обмена данными. Это набор пар "ключ: значение". Все строки должны быть в двойных кавычках.
 
-// в js для задания стилей используется camel case
-box.style.backgroundColor = 'blue';
+//let options = {
+//    width: 1366,
+//    height: 768,
+//    background: 'red',
+//    font: {
+//        size: '16',
+//        color: '#fff'
+//    }
+//};
 
-// вторую кнопку делаем овальной
-btn[1].style.borderRadius = '50%';
+//Если мы хотим отправить этот объект на сервер, нам нужно перевести его в JSON формат. У JSON для этого есть два метода: 
+// stringify - преобразует объект в JSON
+// parse преобразует JSON в объект
 
-//круги превращаем в светофор
-circle[0].style.backgroundColor = 'red';
-circle[1].style.backgroundColor = 'yellow';
-circle[2].style.backgroundColor = 'green';
+//let options = {
+//    width: 1366,
+//    height: 768,
+//    background: 'red',
+//    font: {
+//        size: '16',
+//        color: '#fff'
+//    }
+//};
+//
+//console.log(JSON.stringify(options)); //{"width":1366,"height":768,"background":"red","font":{"size":"16","color":"#fff"}}
 
-//heart.forEach(item => {
-//    item.style.background = 'green';
-//});
+// здесь JSON это встроенный объект в JS
 
-// чтобы создать новый элемент
+// 4.2 AJAX
 
-let div = document.createElement('div'),
-    text = document.createTextNode('text here...');
+let inputRub = document.getElementById('rub'),
+    inputUsd = document.getElementById('usd');
 
-//добавляем класс к созданному элементу
-div.classList.add('black');
+inputRub.addEventListener('input', () => {
+    // input, чтобы прика ждом нажатии применялся обработчик валюты
+    let request = new XMLHttpRequest();
+    // создаем объект XMLHttpRequest и помещаем в переменную для удобства. Это главный объект для работы с запросами. У него есть свои свойства и методы.
+    //request.open(method, url, async, login, pass);
+    // Метод open() производит настройку AJAX-запроса. Этот метод может принимать 5 различных аргументов.
+    // 1 - method, метод по которому общаются клиент и сервер (GET и POST)
+    // 2 - URL - путь к серверу
+    // 3 - отвечает за асинхронность запроса
+    // 4 - login
+    // 5 - pass
 
-//вставляем элемент на страницу
-// document.body.appendChild(div);
+    request.open('GET', 'curren.json');
+    // клиент обращается к серверу по протоколу HTTP. Запросы HTTP также состоят из head и body
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    // настройка запроса
+    request.send();
+    //открывает соединение и отправляет запрос на сервер. Как пример - отправка формы обратной связи на сервер
+    // Это были методы. Свойства бывают - 
 
-// вставлять элементы можно не только в body, но в любой родительский элемент
-wrapper.appendChild(div);
+    // status - содержит http-код ответа сервера, например 404
+    // statusText - ok или not found / response
+    // responseText - текст ответа сервера, по аналогии с магазин это те товары, которые можно купить
+    // readyState - текущее состояние запроса
 
-// вставить элемент до
-document.body.insertBefore(div, circle[0]);
+    request.addEventListener('readystatechange', function () {
+        if (request.readyState === 4 && request.status === 200) {
+            let data = JSON.parse(request.response);
 
-// удаление элемента со страницы
-document.body.removeChild(circle[1]);
-wrapper.removeChild(heart[0]);
-document.body.replaceChild(btn[1], circle[1]);
+            inputUsd.value = inputRub.value / data.usd;
+        } else {
+            inputUsd.value = "Is wrong...";
+        }
+    });
+});
 
-// добавляем текст в эелемент
-div.innerHTML = 'Hello';
-
-// если хотим вставить HTML код, но безопаснее div.textContent = '<h1>Hello</h1>';
-div.innerHTML = '<h1>Hello</h1>';
-
-
-// - выборка элементов на странице
-// - добавление стилей
-// - добавление элементов на страницу
-// - удаление элементов
-// - замена элементов
-// - добавление текста в элементы
+// https://developer.mozilla.org/ru/docs/Web/API/XMLHttpRequest
+//https://developer.mozilla.org/ru/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
+//https://developer.mozilla.org/ru/docs/Web/API/FormData/Using_FormData_Objects
+//https://ilikekillnerds.com/2017/09/convert-formdata-json-object/
+//https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BE%D0%BB%D0%BE%D0%B2%D0%BA%D0%B8_HTTP
